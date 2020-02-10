0a1f21744e640b8022dac28606be5a5613ad19ed
olivier <olibitter@gmail.com>
2020-02-10 15:20:39 +0100

    howmuch formatting

--- commands/howmuch.py
+++ commands/howmuch.py
@@ -2,6 +2,7 @@ import queries as q
 from constants import (
     INVESTMENT_RATE, 
     SEP_DD, 
+    SEP_D,
     IBAN,
     BANK,
     BOOKKEEPER
@@ -23,19 +24,19 @@ def howmuch_command(number, arg_str):
             f"Please transfer {total:.2f} CHF to\n"
             f"Recipient: {BOOKKEEPER}\n"
             f"Bank: {BANK}\n"
-            f"IBAN: {IBAN}"
-            f"\n"
+            f"IBAN: {IBAN}\n"
+            f"{SEP_D}"
             f"Receipt:\n"
             f"Your total expenses:\t{my_expenses:.2f} CHF\n"
             f"Average expenses:\t{avg_expenses:.2f} CHF\n"
             f"Balance:\t\t\t{sign}{abs(balance):.2f} CHF\n"
             f"Amount to transfer is calculated as:\n"
-            f"'Investment Rate' + 'Internet' - 'Balance':"
+            f"Investment + Internet - Balance:\n"
             f"Investement rate:\t{investment:.2f} CHF\n"
-            f"Internet:\t+{internet:.2f} CHF\n"
-            f"Balance:\t{sign_neg}{abs(balance):.2f} CHF\n"
+            f"Internet:\t\t+{internet:.2f} CHF\n"
+            f"Balance:\t\t{sign_neg}{abs(balance):.2f} CHF\n"
             f"{SEP_DD}"
-            f"Total:\t{INVESTMENT_RATE + internet - balance:.2f} CHF"
+            f"Total\t\t\t{total:.2f} CHF"
         )
     else:
         return f"Bills are not ready yet!"
