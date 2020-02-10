6f538808406f11dd8000ec2506a35d7c0a77f22f
olivier <olibitter@gmail.com>
2020-02-10 15:36:51 +0100

    consistent status message

--- commands/status.py
+++ commands/status.py
@@ -6,17 +6,20 @@ def status_command(number, arg_str):
     avg_expenses = q.average_expenses()
     balance = q.flatmate_balance(number)
     internet = q.internet_per_semester()
-    sign = "-:" if balance > 0 else "+"
+    sign = "+" if balance >= 0 else "-"
+    sign_neg = "-" if balance >= 0 else "+"
 
     return (
         f"Your total expenses:\t{my_expenses:.2f} CHF\n"
         f"Average expenses:\t{avg_expenses:.2f} CHF\n"
-        f"Amount to pay by the end of semester:\n"
-        f"Investement rate:\t{INVESTMENT_RATE:.2f} CHF\n"
-        f"Internet:\t{internet:.2f} CHF\n"
         f"Balance:\t\t\t{sign}{abs(balance):.2f} CHF\n"
+        f"Amount to pay by the end of semester:\n"
+        f"Investment + Internet - Balance:\n"
+        f"Investement rate:\t{investment:.2f} CHF\n"
+        f"Internet:\t\t+{internet:.2f} CHF\n"
+        f"Balance:\t\t{sign_neg}{abs(balance):.2f} CHF\n"
         f"{SEP_DD}"
-        f"Total:\t{INVESTMENT_RATE + internet - balance:.2f} CHF"
+        f"Total\t\t\t{total:.2f} CHF"
     )
 
 
